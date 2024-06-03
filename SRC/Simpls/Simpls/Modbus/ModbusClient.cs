13ec64f0ca148e9514b2299e4c8a683ec34311d2
popo <popo_520@outlook.com>
2024-06-03 17:52:23 +0800

    fix

--- SRC/Simpls/Simpls/Modbus/ModbusClient.cs
+++ SRC/Simpls/Simpls/Modbus/ModbusClient.cs
@@ -32,19 +32,16 @@ public class ModbusClient : IDisposable
         return this.Read<bool>(slave, address, 1, master => master.ReadCoils)[0];
     }
 
-    static IEnumerable<ushort[]> GroupByPairs(ushort[] source)
+    private static T[][] GroupArray<T>(T[] array, int groupSize)
     {
-        for (int i = 0; i < source.Length; i += 2)
-        {
-            if (i + 1 < source.Length)
-            {
-                yield return new[] { source[i], source[i + 1] };
-            }
-            else
-            {
-                yield return new[] { source[i] };
-            }
-        }
+        if (groupSize <= 0)
+            throw new ArgumentException("Group size must be a positive integer.");
+
+        var groups = array.Select((item, index) => new { item, groupIndex = index / groupSize })
+                           .GroupBy(x => x.groupIndex)
+                           .Select(g => g.Select(x => x.item).ToArray())
+                           .ToArray();
+        return groups;
     }
 
     public float[] ReadSingles(ushort address, ushort readCount, byte slave = 1)
@@ -52,7 +49,7 @@ public class ModbusClient : IDisposable
         var array = new float[readCount];
         //float是两位读取
         var source = this.ReadNumbers(address, (ushort)(readCount * 2), slave);
-        var pairs = GroupByPairs(source).ToArray();
+        var pairs = GroupArray(source, 2).ToArray();
         for (int i = 0; i < array.Length; i++)
         {
             var item = pairs[i];
@@ -128,6 +125,114 @@ public class ModbusClient : IDisposable
             connection.Reset();
             pool.ReturnClient(connection);
         }
+    }
+
+    private ushort[] GetUShorts(float value)
+    {
+        byte[] floatBytes = BitConverter.GetBytes(value);
+
+        // 检查系统默认的字节序，并可能需要反转字节顺序以适应Modbus设备的大端字节序  
+        if (BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(floatBytes);
+        }
+
+        // 如果你的设备需要两个独立的ushort值而不是一个连续的字节数组，  
+        // 你可以使用Buffer.BlockCopy来提取这两个ushort  
+        ushort[] registers = new ushort[2];
+        Buffer.BlockCopy(floatBytes, 0, registers, 0, sizeof(ushort) * 2);
+
+        // 但是，上面的Buffer.BlockCopy调用不会按你期望的方式工作，因为它试图将整个floatBytes数组  
+        // 复制到registers数组中，这会导致溢出并且不会正确地分离出两个ushort值。  
+        // 因此，你应该手动提取这两个ushort值。  
+
+        registers[0] = (ushort)((floatBytes[0] << 8) | floatBytes[1]); // 假设大端字节序  
+        registers[1] = (ushort)((floatBytes[2] << 8) | floatBytes[3]);
+        return registers;
+    }
+
+    public void WriteSingle(ushort address,float value,byte slave=1)
+    {
+        var registers = this.GetUShorts(value);
+        this.Write(master => {
+            master.WriteMultipleRegisters(slave, address, registers);
+        });
+    }
+
+    public void WriteSingles(ushort address, float[] data, byte slave = 1)
+    {
+        var list = new List<ushort>();
+        for (int i = 0; i < data.Length; i++)
+        {
+            var registers = this.GetUShorts(data[i]);
+            foreach (var register in registers)
+            {
+                list.Add(register);
+            }
+        }
+        this.Write(master => {
+            master.WriteMultipleRegisters(slave, address, list.ToArray());
+        });
+    }
+
+    private ushort[] GetUShorts(double value)
+    {
+        byte[] doubleBytes = BitConverter.GetBytes(value);
+
+        if (BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(doubleBytes);
+        }
+
+        ushort[] ushortValues = new ushort[doubleBytes.Length / sizeof(ushort)];
+        Buffer.BlockCopy(doubleBytes, 0, ushortValues, 0, doubleBytes.Length);
+
+        for (int i = 0; i < ushortValues.Length; i++)
+        {
+            ushortValues[i] = (ushort)((doubleBytes[i * 2] << 8) | doubleBytes[i * 2 + 1]);
+        }
+        return ushortValues;
+    }
+
+    public void WriteDouble(ushort address, double value,byte slave=1)
+    {
+        var shorts = this.GetUShorts(value);
+        this.Write(master => master.WriteMultipleRegisters(slave, address, shorts));
+    }
+
+    public void WriteDoubles(ushort address, double[] data,byte slave = 1)
+    {
+        var list = new List<ushort>();
+        for (int i = 0; i < data.Length; i++)
+        {
+            var registers = this.GetUShorts(data[i]);
+            foreach (var register in registers)
+            {
+                list.Add(register);
+            }
+        }
+        this.Write(master => {
+            master.WriteMultipleRegisters(slave, address, list.ToArray());
+        });
+    }
+
+    public double ReadDouble(ushort address, byte slave = 1)
+    {
+        var source = this.ReadNumbers(address, 4, slave);
+        return Modbus.Utility.ModbusUtility.GetDouble(source[0], source[1], source[2], source[3]);
+    }
+
+    public double[] ReadDoubles(ushort address,int readCount, byte slave = 1) 
+    {
+        var array = new double[readCount];
+        var source = this.ReadNumbers(address, (ushort)(readCount * 4), slave);
+        var pairs = GroupArray(source, 4);
+        for (int i = 0; i < array.Length; i++)
+        {
+            var item = pairs[i];
+            array[i] = Modbus.Utility.ModbusUtility.GetDouble(item[0], item[1], item[2], item[3]);
+        }
+        return array;
     }
 
     public void Dispose()
--- SRC/Simpls/Simpls/ModbusTest.cs
+++ SRC/Simpls/Simpls/ModbusTest.cs
@@ -11,9 +11,16 @@ namespace Simpls
         {
             using (var modbus = new ModbusClient("127.0.01", 502))
             {
-                var number = modbus.ReadSingles(40000, 1);
+                //modbus.WriteSingles(40000, [0.12f, 0.13f, 0.14f]);
+                //var pi = modbus.ReadSingles(40000, 3);
+                //modbus.WriteDouble(40000,3.1415926);
+                //modbus.WriteDoubles(40000, [3.1415926d, 3.1415925d, 3.1415924d]);
+                //var xx= modbus.ReadDoubles(40000, 3);
+                //var value = modbus.ReadDouble(40000);
 
-                Console.WriteLine(number);
+                var a = sizeof(float);
+                var d= sizeof(decimal);
+                var x = sizeof(double);
             }
 
         }
